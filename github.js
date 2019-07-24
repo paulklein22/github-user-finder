63efb3afcf7ce81ee1d792b69e7131557d1512a7
PaulKlein22 <paulklein22@gmail.com>
2019-07-24 02:15:12 -0500

    UI and alert

--- github.js
+++ github.js
@@ -1,10 +1,20 @@
-class GitHub {
+class Github {
   constructor() {
     this.client_id = 'b34beb65fd2796b7e697';
     this.client_secret = 'a092c7b03813d278de154abadd284c6e4a5ff46a';
   }
 
   async getUser(user) {
-    const profileResponse
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    };
   }
 }
